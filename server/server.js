e2dcb94be62df2422e0bbc3d2806647babaf28f7
vikasvishwagna <vishwagnavikassai@gmail.com>
2025-12-19 01:58:11 +0530

    fix:serinitial page

--- server/server.js
+++ server/server.js
@@ -7,6 +7,10 @@ connectDB();
 
 const PORT = process.env.PORT || 5000;
 
+app.get("/", (req, res) => {
+  res.send("BookMyEvent API is running!");
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
